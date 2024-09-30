442d5d0cc023618df18bfa0dd7828541370c71f3
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-09-30 20:24:31 +0600

    a

--- javaScript/myscript.js
+++ javaScript/myscript.js
@@ -1,2 +1,8 @@
 alert("hello world");
+<<<<<<< Updated upstream
 console.log("hello world");
+=======
+var deposit=prompt("How much would you like to deposit?");//prompt is a function that takes input from the user
+alert("You've deposited: "+deposit);
+console.log("You are a cool person");
+>>>>>>> Stashed changes
