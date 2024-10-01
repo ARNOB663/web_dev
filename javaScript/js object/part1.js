8d7a3bb23b8a09c5d023f03197df79b5987450b2
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-10-01 10:01:14 +0600

    a

---
+++ javaScript/js object/part1.js
@@ -0,0 +1,4 @@
+ var carinfo = {make: "toyota",year: 1990 , model: "camry"};
+  carinfo; // {make: "toyota", year: 1990, model: "camry"}
+  console.log(carinfo); // {make: "toyota", year: 1990, model: "camry"}
+  console.log(carinfo.make); // toyota
