c7d01d074c1193849bf42a7747ae999241ca4149
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-10-02 20:22:16 +0600

    a

--- javaScript/DOM(Document Object Model)/part1/script.js
+++ javaScript/DOM(Document Object Model)/part1/script.js
@@ -1,1 +1,16 @@
-document.getElementsByid('pickme');
+var myheader= document.querySelector('h1');
+myheader.style.color='red';
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  function changeHeaderColor(){
+      colorInput = getRandomColor();
+      myheader.style.color = colorInput;
+  }
+    setInterval("changeHeaderColor()",500);
+    myheader.addEventListener("mouseover",changeHeaderColor);//mouseover is an event listener that listens for the mouse to hover over the header 
