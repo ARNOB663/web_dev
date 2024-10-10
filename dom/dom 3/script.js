a0c991a239650a200f827aa7d182b745c90780e1
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-10-10 21:07:29 +0600

    Update script.js

--- dom/dom 3/script.js
+++ dom/dom 3/script.js
@@ -3,3 +3,12 @@ document.getElementById("clickMeButton").addEventListener('mouseover', function(
 
     alert("chaicode");
 });   
+//example 7
+
+document.getElementById("tealist")
+.addEventListener('click', function(event){
+
+if(event.target && event.target.matches('teaItem')){
+    alert("chaicode");
+}
+});
