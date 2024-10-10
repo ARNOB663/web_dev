6aaffca3ca16a10f8aafcfbb34277a3c1ac7512e
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-10-10 11:12:22 +0600

    a

--- dom/dom 2/script.js
+++ dom/dom 2/script.js
@@ -1,1 +1,13 @@
 //exmaple 1
+// let hold = document.getElementById("changeTextButton");
+// console.log(hold);
+document.getElementById("changeTextButton").addEventListener("click",function () { //this is an event listener that listens for a click event on the button with the id of changeTextButton
+
+console.log(this);
+
+});
+document.getElementById("changeTextButton").addEventListener("click",()=>    { //this is an event listener that listens for a click event on the button with the id of changeTextButton
+    document.getElementById("myParagraph")
+    
+    });
+
