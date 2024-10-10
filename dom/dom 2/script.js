f6e6b4751cd4d9b031ce2e521679c7dbb2b25069
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-10-10 11:19:06 +0600

    Update script.js

--- dom/dom 2/script.js
+++ dom/dom 2/script.js
@@ -8,8 +8,9 @@ console.log(this);
 });
 document.getElementById("changeTextButton").addEventListener("click",()=>    { //this is an event listener that listens for a click event on the button with the id of changeTextButton
     let paragraph=document.getElementById("myParagraph");
-    console.log(paragraph);// this will log the paragraph element
-    console.log(paragraph.innerHTML);//this will log the innerHTML of the paragraph
+    //console.log(paragraph);// this will log the paragraph element
+    //console.log(paragraph.innerHTML);//this will log the innerHTML of the paragraph
+    paragraph.innerHTML="this paragraph is changed";//this will change the innerHTML of the paragraph
 
     
     });
