24604c9fd852adddc16e43d03fd64fef5b598d5c
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-10-19 10:20:42 +0600

    funtion

---
+++ javaScript/function.js
@@ -0,0 +1,25 @@
+//function
+//funtion declaration 
+function cheak_result(result){//variable
+if(result>=80){
+    console.log("A+");
+}
+    else if(result>=70){
+        console.log("A");
+    }
+    else if(result>=60){
+        console.log("B");
+    }
+    else if(result>=50){
+        console.log("C");
+    }
+    else if(result>=40){
+        console.log("D");
+    }
+    else{
+        console.log("Fail");
+    }
+}
+cheak_result(90);
+cheak_result(80);
+cheak_result(70);
