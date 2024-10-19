7c6ceff2a5dc7bfc23ca296b4120b614a0fc134d
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-10-19 10:59:51 +0600

    Update function.js

--- javaScript/function.js
+++ javaScript/function.js
@@ -63,4 +63,53 @@ console.log(multiply(2,3,4,5));
 //arrow function
 
 //call back funtion
+function greeting(name){
+    console.log("Hello "+name);
+}
+function processUserInput(callback){
+    var name = "Arafat";
+    callback(name);
+}
+processUserInput(greeting);
+//write an call back funtion
 
+const add1 = function(a,b,callback){
+    return callback(a+b);
+}
+const callback = function(result){
+    return result;
+}
+console.log(add1(4,5,callback));
+
+//write an call back funtion
+
+const add2 = function(a,b,c){
+
+    return a+b+c;
+}
+
+const devide = function(result){
+
+    return result/2;
+}
+console.log(add(4,5,devide));
+
+//ticket booking system using call back funtion
+const bookTicket = function(name,price,callback){
+    return callback(name,price);
+}
+//
+const callback1 = function(name,price){
+    console.log("Ticket booked for "+name+" and price is "+price);
+}
+bookTicket("Arafat",500,callback1);
+// log in cheak seat avilable or not booking and payment system using call back funtion
+const logIn = function(email,password,callback){
+    return callback(email,password);
+}
+const callback2 = function(email,password){
+    if(email=="" || password
+    ==""){
+        console.log("Please enter your email and password");
+    }
+}
