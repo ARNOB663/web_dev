526169b4c922643fd006c25b76725211de6069cb
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-11-03 23:15:08 +0600

    Update aync_awaite2.js

--- dom/Aync awit/aync_awaite2.js
+++ dom/Aync awit/aync_awaite2.js
@@ -2,7 +2,31 @@ function fetchPostData(){
 
 return new Promise((resolve, reject) => {
     setTimeout(() => {
-        
-    }, 1000);
+         resolve("post fetched successfully")
+    },  1000);
 });
 }
+function fetchCommentsData(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+             resolve("comments fetched successfully")
+        },  1000);
+    });
+
+}
+
+
+async function getBlogData(){
+try {
+     console.log("fetching  blog data ");
+    //  let blogData=await fetchPostData();
+    //  let commentsData=await fetchCommentsData();
+     const [PostData,commentsData] = await Promise.all([fetchPostData(),fetchCommentsData()])
+        console.log("blog data",PostData);
+        console.log("comments data",commentsData);
+} catch (error) {
+    console.log("error fetching data",error);
+}
+
+}
+getBlogData();
