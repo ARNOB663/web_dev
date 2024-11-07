514dd4f789530c5f07d298a5b7ebfec631ae5187
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-11-07 20:19:35 +0600

    a

---
+++ dom/ES6 modules and common js/appC.js
@@ -0,0 +1,3 @@
+ const mathOperationM = require("./mathoparatorC.cjs");
+
+console.log(mathOperationM.multiply(2,3)); //6
---
+++ dom/ES6 modules and common js/appM.js
@@ -0,0 +1,9 @@
+//default import
+import multiply from './mathOperationM.js';
+//named import
+import {add,sub} from './mathOperationM.js';
+
+console.log(multiply(2,3)); //6
+console.log(add(2,3)); //5
+console.log(sub(2,3)); //-1
+//Module import file
